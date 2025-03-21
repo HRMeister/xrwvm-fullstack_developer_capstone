445b35b77b8120fcdd1e07353fb5edda240b8222
HRMeister <hugorlmeister@gmail.com>
2025-03-21 17:20:31 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -18,21 +18,22 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealer_id = models.IntegerField(default=1)
     CAR_TYPES = [
-        ('SEDAN','Sedan'), 
-        ('SUV', 'SUV'), 
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
         ('WAGON', 'Wagon')
         ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default = 'SUV')
-    year = models.IntegerField(default=2024,
-        validators = [
-           MaxValueValidator(2024), 
-           MinValueValidator(2015)
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=2024,
+        validators=[
+            MaxValueValidator(2024),
+            MinValueValidator(2015)
         ])
     COLORS = [
-        ('red','RED'),
-        ('black','BLACK'),
-        ('white','WHITE'),
-        ('blue','BLUE')
+        ('red', 'RED'),
+        ('black', 'BLACK'),
+        ('white', 'WHITE'),
+        ('blue', 'BLUE')
     ]
     color = models.CharField(max_length=10, choices=COLORS, default='white')
 
