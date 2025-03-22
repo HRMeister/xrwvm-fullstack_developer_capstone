a3ebe12cac3a77fa9e73a1193a3a0d8146e13764
HRMeister <hugorlmeister@gmail.com>
2025-03-21 18:19:41 -0600

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     # path for logout
-    path(route='logout',v iew=views.logout_request, name='logout'),
+    path(route='logout',view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
     path(route='get_dealers/', view=views.get_dealerships,
