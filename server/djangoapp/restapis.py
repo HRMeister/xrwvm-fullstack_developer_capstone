4503558a614023d12d647903faacdc1e605a18d2
HRMeister <hugorlmeister@gmail.com>
2025-03-21 18:32:26 -0600

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -17,7 +17,7 @@ def get_request(endpoint, **kwargs):
     if (kwargs):
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
-            
+
     request_url = backend_url+endpoint+"?"+params
     print("GET from {} ".format(request_url))
     try:
@@ -45,4 +45,3 @@ def post_review(data_dict):
         return response.json()
     except Exception as e:
         print(f"Network exception occurred: {e}")
-        
